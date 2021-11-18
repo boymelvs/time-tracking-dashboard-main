dfecf9c23282d4d34a855b7726972801d3ff5dab
boymelvs <ambo_dacoycoy@yahoo.com>
2021-11-18 14:54:41 +0300

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,64 @@
 const mainContent = document.querySelector(".main_content");
-const card = document.querySelector(".card");
+const card = document.getElementById("card");
 
-// const cardClone = card.cloneNode(true);
+let cardElem = `
+            <div class="card work">
+               <div class="tracker">
+                  <div class="title_ellipsis_container">
+                     <h2 class="title"></h2>
+                     <img
+                        src="./images/icon-ellipsis.svg"
+                        alt="ellipsis"
+                        class="icon_ellipsis"
+                     />
+                  </div>
 
-// const test = (card) => {
-//    const tet = mainContent.appendChild(card);
+                  <div class="time_container">
+                     <p class="current">
+                        <span class="current_time">32</span>hrs
+                     </p>
 
-//    const ewan = (tet.querySelector(".title").innerHTML = "Play");
+                     <p class="previous">
+                        <span class="prev">Last Week</span> -
+                        <span class="prev_time">36</span>hrs
+                     </p>
+                  </div>
+               </div>
+            </div>`;
 
-//    console.log("this is", ewan);
-// };
+const insertCard = (elem, data) => {
+   const newCard = card.cloneNode(true);
 
-// test(cardClone);
+   data.then((value) => {
+      for (let i = 0; i < 6; i++) {
+         mainContent.appendChild(newCard);
+         const card = elem.classList.add(
+            value[i].title.replace(/\s/g, "").toLowerCase()
+         );
 
-const getData = () => {
-   fetch("../data.json")
-      .then((res) => {
-         return res.json();
-      })
-      .then((data) => {
-         console.log("this data", data);
-         console.log("this title", data[1].title);
-         console.log("this timeframe", data[1].timeframes.daily);
-         console.log("this current", data[1].timeframes.daily.current);
-      });
+         console.log("this is title", elem);
+      }
+   });
+   // const ewan = (mainContent.querySelector(".title").innerHTML = "Work");
+
+   // const moveItems = document.querySelectorAll(".to-be-moved");
+   // let targetElements = document.querySelectorAll(".wine-gallery-wrap");
+
+   // for (let i = 0; i < targetElements.length; i++) {
+   //    targetElements[i].appendChild(moveItems[i]);
+   // }
 };
 
-getData();
+const getData = () => {
+   return fetch("../data.json").then((res) => res.json());
+
+   // .then((data) => {
+   //    return data;
+   // });
+
+   // console.log("this title", data[1].title);
+   // console.log("this timeframe", data[1].timeframes.daily);
+   // console.log("this current", data[1].timeframes.daily.current);
+};
+
+insertCard(cardElem, getData());
