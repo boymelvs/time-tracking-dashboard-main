fadd34320064a812f26a8dd2b13cd98cf82b43f7
boymelvs <ambo_dacoycoy@yahoo.com>
2021-11-18 23:30:22 +0300

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,7 @@
 const mainContent = document.querySelector(".main_content");
-const card = document.getElementById("card");
 
 let cardElem = `
-            <div class="card work">
+            <div class="card ">
                <div class="tracker">
                   <div class="title_ellipsis_container">
                      <h2 class="title"></h2>
@@ -15,50 +14,79 @@ let cardElem = `
 
                   <div class="time_container">
                      <p class="current">
-                        <span class="current_time">32</span>hrs
+                        <span class="current_time"></span>
                      </p>
 
                      <p class="previous">
-                        <span class="prev">Last Week</span> -
-                        <span class="prev_time">36</span>hrs
+                        <span class="prev"></span> -
+                        <span class="prev_time"></span>
                      </p>
                   </div>
                </div>
             </div>`;
 
-const insertCard = (elem, data) => {
-   const newCard = card.cloneNode(true);
+/* insert card to DOM */
+const insertCard = (length) => {
+   for (let i = 0; i < length; i++) {
+      mainContent.innerHTML += cardElem;
+   }
+};
 
-   data.then((value) => {
-      for (let i = 0; i < 6; i++) {
-         mainContent.appendChild(newCard);
-         const card = elem.classList.add(
-            value[i].title.replace(/\s/g, "").toLowerCase()
-         );
+/* ================= inserting data start here ================= */
+const insertData = (data, time) => {
+   const cards = document.querySelectorAll(".card");
 
-         console.log("this is title", elem);
+   cards.forEach((card, key) => {
+      /* insert tracker title */
+      card.classList.add(data[key].title.replace(/\s/g, "").toLowerCase());
+      card.querySelector(".title").innerHTML = data[key].title;
+
+      /* insert current time */
+      const myUnit = data[key].timeframes[time].current > 1 ? "hrs" : "hr";
+      card.querySelector(".current_time").innerHTML = data[key].timeframes[time].current + myUnit;
+
+      /* insert if yesterday, last week/month */
+      const itHappenWhen = time === "daily" ? "Yesterday" : time === "weekly" ? "Last Week" : "Last Month";
+      card.querySelector(".prev").innerHTML = itHappenWhen;
+
+      /* insert previous time */
+      const unit = data[key].timeframes[time].previous > 1 ? "hrs" : "hr";
+      card.querySelector(".prev_time").innerHTML = data[key].timeframes[time].previous + unit;
+   });
+};
+
+/* activate classes once click */
+const isActive = (timeframes, index) => {
+   timeframes.forEach((timeframe, key) => {
+      if (index === key) {
+         timeframe.classList.add("active");
+      } else {
+         timeframe.classList.remove("active");
       }
    });
-   // const ewan = (mainContent.querySelector(".title").innerHTML = "Work");
-
-   // const moveItems = document.querySelectorAll(".to-be-moved");
-   // let targetElements = document.querySelectorAll(".wine-gallery-wrap");
-
-   // for (let i = 0; i < targetElements.length; i++) {
-   //    targetElements[i].appendChild(moveItems[i]);
-   // }
 };
 
-const getData = () => {
-   return fetch("../data.json").then((res) => res.json());
+/* ================= getting data here ================= */
+const getData = async () => {
+   const res = await fetch("../data.json");
+   const data = await res.json().then((data) => {
+      return data;
+   });
 
-   // .then((data) => {
-   //    return data;
-   // });
+   insertCard(data.length); /* call insertCard fn */
 
-   // console.log("this title", data[1].title);
-   // console.log("this timeframe", data[1].timeframes.daily);
-   // console.log("this current", data[1].timeframes.daily.current);
+   insertData(data, "weekly"); /* call insertData fn */
+
+   /* waiting for click and become active */
+   const timeframes = document.querySelectorAll(".timeframe");
+
+   timeframes.forEach((timeframe, index) => {
+      timeframe.addEventListener("click", (event) => {
+         const time = event.target.innerText.toLowerCase();
+         insertData(data, time);
+         isActive(timeframes, index);
+      });
+   });
 };
 
-insertCard(cardElem, getData());
+getData(); /* get data onload */
